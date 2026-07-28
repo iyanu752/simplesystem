ad0c9c9cdd8de8ef258e9fa55cdc9641b04d1625
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2026-07-28 15:21:08 +0100

    migration: database migration and update

---
+++ server/Migrations/20260728141917_AddEdgeTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SimpleSystem.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEdgeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Edges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomId = table.Column<string>(type: "text", nullable: false),
+                    SourceNodeId = table.Column<int>(type: "integer", nullable: false),
+                    TargetNodeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Edges", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Edges");
+        }
+    }
+}
